a32260de7abfc80cae9ba37dabd0be0998b33e52
Peik Etzell <peik.etzell@aalto.fi>
2020-12-06 06:17:09 +0200

    Changed back to using Vector instead of Buffer

--- src/main/scala/DemoCube/Camera.scala
+++ src/main/scala/DemoCube/Camera.scala
@@ -7,7 +7,7 @@ class Camera {
   var head = new Point(0, 0, - 10 * WorldSize)
   val headVectorSize = 1000
   val vectors = Vector(new Vec(headVectorSize, 0, 0), new Vec(0, headVectorSize, 0), new Vec(0, 0, headVectorSize))
-  var lines = Buffer[Line]()
+  var lines = Vector[Line]()
 
   val background = rectangle(ViewSize, ViewSize, BackGroundColor)
 
@@ -15,14 +15,12 @@ class Camera {
     var view = background
     for (line <- lines) {
       view = view.place(line.toPic(vectors, head), line.a.relativePos(vectors, head))
-
     }
-    this.lines.clear()
     view
   }
 
-  def addLines(lines: Vector[Line]): Unit = {
-    this.lines.appendAll(lines)
+  def updateLines(lines: Vector[Line]): Unit = {
+    this.lines = lines
   }
 
   def move(vec: Vec) = {
